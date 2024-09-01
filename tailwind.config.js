7e7ba37714a0fcd62ee0ea75c7ffefe6dae9469a
ilyeshr2 <ilyesharrache0@gmail.com>
2024-09-01 23:25:11 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'system': ['-apple-system', 'BlinkMacSystemFont', 'sans-serif'],
+      },
+      colors: {
+        'custom-gray': '#a3a3a3',
+        'custom-gray2': '#808080',
+      },
+    },
+  },
+  plugins: [],
+}
